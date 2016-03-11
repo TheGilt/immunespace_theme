d24b4966f4277bf6c57a639dbfafe6041a67f712
TheGilt <gilguday@outlook.com>
2016-03-11 11:45:49 -0800

    Updated Axis lines to account for potential error

--- R/theme_immune_space.R
+++ R/theme_immune_space.R
@@ -18,7 +18,8 @@ theme_immune_space <- function() {
     theme(panel.grid.major = element_line(colour = "#ded8d5", linetype = "dotted")) +
     theme(panel.grid.minor = element_line(colour = "#FDF6E3", linetype = "dotted")) +
     theme(axis.ticks = element_line(size = 0)) +
-    theme(axis.line = element_line(size = 0.5, colour = "black")) +
+    theme(axis.line.x = element_line(size = 0.5, colour = "black")) +
+    theme(axis.line.y = element_line(size = 0.5, colour = "black")) +
     theme(plot.title = element_text(size = rel(1))) +
     theme(strip.background = element_rect(colour = "white", fill = "white"))
 }
